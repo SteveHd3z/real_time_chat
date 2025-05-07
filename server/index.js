4535f6417eca7128494e7033522e4bd914f5c651
Steve <edxteve@gmail.com>
2025-05-06 19:22:14 -0500

    Conexion a web Socket y envío de valores

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
 import express from 'express';
+import logger from 'morgan';
 
-const port= proccess.env.PORT || 3000;
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+
+
+const port= process.env.PORT || 3000;
 
 const app = express();
+const server = createServer(app);
+const io= new Server(server)
 
-app.get('/', (req, res) => {
-    res.send('<h1>Esto es el chat</h1>')   
+io.on('connection', (socket) => {
+    console.log('A user has connected')
+
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected')
+    })
 })
 
-app.listen(port, () => {
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html')  
+})
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
