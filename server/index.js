e88467f9cf5fb63c516594acb5a4b87e76b073d3
SteveHd3z <edxteve@gmail..com>
2025-05-07 15:30:53 -0500

    Implementación de persistencia de datos con database turso

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import logger from 'morgan';
+import dotenv from 'dotenv';    //dotenv es un modulo que nos permite cargar variables de entorno desde un archivo .env
+import { createClient } from '@libsql/client'; //importamos el cliente de libsql para poder usarlo en el servidor
 
 import { Server } from 'socket.io';//importamos socket.io para poder usarlo en el servidor
 //importamos el servidor http para poder usarlo con socket.io
 import { createServer } from 'http';
+
+dotenv.config(); //cargamos las variables de entorno desde el archivo .env
 
 //Puerto que vamos a utilizar
 const port= process.env.PORT || 3000;
@@ -19,6 +23,15 @@ const io= new Server(server, {
     connectionStateRecovery:{}
 })
 
+const db=createClient({
+    url:"libsql://db-chat-stevehd3z.aws-us-east-1.turso.io",
+    authToken:process.env.DB_TOKEN,
+})
+
+await db.execute(`CREATE TABLE IF NOT EXISTS messages 
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+     content TEXT)`)
+
 //Configuramos el socket.io para que escuche las conexiones
 //cuando un cliente se conecta, se ejecuta la funcion de callback
 io.on('connection', (socket) => {
@@ -28,9 +41,20 @@ io.on('connection', (socket) => {
         console.log('A user has disconnected')
     })
     
-    socket.on('chat message', (msg) => {        //cuando el cliente envia un mensaje, se ejecuta la funcion de callback
-        console.log('Message received:', msg)
-        io.emit('chat message', msg)            //emitimos el mensaje a todos los clientes conectados
+    socket.on('chat message', async (msg) => {        //cuando el cliente envia un mensaje, se ejecuta la funcion de callback
+        let result
+        try{
+            result= await db.execute({
+                sql: `INSERT INTO messages (content) VALUES (:msg)`,
+                args: {msg} //insertamos el mensaje en la base de datos
+        }) //insertamos el mensaje en la base de datos
+
+        }catch(e){
+            console.error(e)
+            return
+        }
+
+        io.emit('chat message', msg, result.lastInsertRowid.toString())            //emitimos el mensaje a todos los clientes conectados
     })
 })
 
